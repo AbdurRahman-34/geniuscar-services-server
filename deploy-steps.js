ecb4a94b33fe593bcd07e32dc2241f7d6495384e
AbdurRahman-34 <get.abdurahman@gmail.com>
2023-04-10 00:01:53 +0600

    versel code update

---
+++ deploy-steps.js
@@ -0,0 +1,16 @@
+/*
+1. npm install -g vercel / frist time 
+
+2 . vercel login / frist time
+
+2. add vercel config  file
+
+3. vercel / y   :meaning deploy 
+
+4. add :  "build": "node index.js" in your package.json file
+
+5. production link deploy link : 
+
+6. Environment Variables set : deshboard --> projects --> setting --> Environment Variables
+
+*/
